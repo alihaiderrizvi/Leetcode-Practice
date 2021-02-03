631d8ce44943bf80917a28059a561f1e70c30fe9
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2021-02-03 02:15:08 -0800

    Delete 171-Excel Sheet Column Number directory

--- 171-Excel Sheet Column Number/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def titleToNumber(self, s: str) -> int:
-        a = 0
-        power = 0
-        for x in s[::-1]:
-            a += (ord(x)-64)*(26**power)
-            power+= 1
-        return a
