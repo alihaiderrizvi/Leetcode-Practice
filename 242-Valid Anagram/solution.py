d7deb77ed7265ac6dc0fa8d1bf4baa1a2baecafe
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2021-02-03 02:15:19 -0800

    Delete 242-Valid Anagram directory

--- 242-Valid Anagram/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        t = list(t)
-        s = list(s)
-        t = sorted(t)
-        s = sorted(s)
-        if s == t:
-            return True
-        return False
