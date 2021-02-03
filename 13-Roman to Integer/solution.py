c170f62df1e1a65cbfc8f4057abda77b6b1cd292
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2021-02-03 01:29:25 -0800

    Add files via upload

---
+++ 13-Roman to Integer/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        numbers = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        num_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        summ = 0
+        
+        for i in range(len(s)-1):
+            
+            if numbers.index(s[i+1]) > numbers.index(s[i]):
+                summ -= num_dict[s[i]]
+            else:
+                summ += num_dict[s[i]]
+        
+        summ += num_dict[s[-1]]
+        
+        return summ
